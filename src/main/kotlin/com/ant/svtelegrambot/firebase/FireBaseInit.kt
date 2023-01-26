00b981f7c886fa05b43169216981ba8e45f88f5c
ant <theant@mail.ru>
2023-01-26 07:13:18 +0400

    вынес константы

--- src/main/kotlin/com/ant/svtelegrambot/firebase/FireBaseInit.kt
+++ src/main/kotlin/com/ant/svtelegrambot/firebase/FireBaseInit.kt
@@ -6,6 +6,9 @@ import com.google.firebase.FirebaseOptions
 import org.springframework.stereotype.Component
 import java.io.FileInputStream
 
+private const val BASE_URL = "https://vsmuta-f3e3d-default-rtdb.europe-west1.firebasedatabase.app"
+private const val TOKEN_FILE = "/home/ant/IdeaProjects/svtelegrambot/src/main/resources/vsmuta-f3e3d-firebase-adminsdk-3dzma-12edada204.json"
+
 @Component
 class FireBaseInit {
 //    @Value("\${firebase.tokenfile}")
@@ -14,16 +17,12 @@ class FireBaseInit {
 //    @Value("\${firebase.baseurl}")
 //    private val baseUrl: String? = null
 
-    val baseUrl = "https://vsmuta-f3e3d-default-rtdb.europe-west1.firebasedatabase.app"
-    val tokenFile = "/home/ant/IdeaProjects/svtelegrambot/src/main/resources/vsmuta-f3e3d-firebase-adminsdk-3dzma-12edada204.json"
-
-
-    private val serviceAccount = FileInputStream(tokenFile)
+   private val serviceAccount = FileInputStream(TOKEN_FILE)
 
     fun firebaseInit() {
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .setDatabaseUrl(baseUrl)
+            .setDatabaseUrl(BASE_URL)
             .build()
         FirebaseApp.initializeApp(options)
     }
